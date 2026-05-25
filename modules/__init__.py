f45b42ef6f6791e1adf422a94d5b9d95bde0457b
consultorabiocore <consultorabiocore@gmail.com>
2026-05-25 18:30:51 -0400

    Add initial module definition for DarwinCheck
    Initial module setup for DarwinCheck with version and author information.

---
+++ modules/__init__.py
@@ -0,0 +1,19 @@
+# ============================================================================ #
+#                         DarwinCheck - Módulos                               #
+# ============================================================================ #
+
+"""
+Paquete de módulos para DarwinCheck Vol.1
+"""
+
+__version__ = "1.0.0"
+__author__ = "Loreto Campos (BioCore)"
+__all__ = [
+    'config',
+    'utils',
+    'taxonomia',
+    'coordenadas',
+    'ecologia',
+    'graficos',
+    'excel_io'
+]
